e65b6d525144abd94cc78e5508ac250f42e27d38
Chris Corriveau <genevate@users.noreply.github.com>
2018-07-13 18:36:07 -0500

    changed the number format from en-US to us

--- public/javascripts/store.js
+++ public/javascripts/store.js
@@ -169,7 +169,7 @@ class Store {
   // Format a price (assuming a two-decimal currency like EUR or USD for simplicity).
   formatPrice(amount, currency) {
     let price = (amount / 100).toFixed(2);
-    let numberFormat = new Intl.NumberFormat(['en-US'], {
+    let numberFormat = new Intl.NumberFormat(['US'], {
       style: 'currency',
       currency: currency,
       currencyDisplay: 'symbol',
